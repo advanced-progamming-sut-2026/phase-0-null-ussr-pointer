17ba42eb7f03ca5d44d6e68782e39f03dee6eb38
rezahazrati <rhazrati44@gmail.com>
2026-06-07 12:36:47 +0330

    from reza: pleas make a Vec2 class so we can keep our entities position and possible velocity as a Vec2 instead of pure x y inside game entity so change that

--- src/model/account/AccountState.java
+++ src/model/account/AccountState.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 public record AccountState(String username , String password , String email , int currentLvl , int coin , int gem , int score , Map<String , Integer> plantLvl , List<NewsItem> personalNews) {
-    public static AccountState createNewPlayer(String username , String password , String email){}
+    public static AccountState createNewPlayer(String username , String password , String email){
+        return null;
+    }
     public AccountState {
         //in this method we can check the validations
     }
