e82046c5cef16c5df7ba7d580d663088aad8d250
rezahazrati <rhazrati44@gmail.com>
2026-06-08 05:50:55 +0330

    changed Vec2 from a final class to a record

--- src/model/util/Vec2.java
+++ src/model/util/Vec2.java
@@ -1,23 +1,19 @@
 package model.util;
 
-public final class Vec2 {
-    public final double x;
-    public final double y;
-
-    public Vec2(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+public record Vec2(double x, double y) {
 
     public Vec2 add(Vec2 v) {
         return null;
     }
+
     public Vec2 sub(Vec2 v) {
         return null;
     }
+
     public Vec2 scale(double d) {
         return null;
     }
+
     public Vec2 negate() {
         return null;
     }
@@ -25,12 +21,15 @@ public final class Vec2 {
     public double length() {
         return 0;
     }
+
     public Vec2 normalize() {
         return null;
     }
+
     public double distanceTo(Vec2 v) {
         return 0;
     }
+
     public double dot(Vec2 v) {
         return 0;
     }
@@ -38,6 +37,7 @@ public final class Vec2 {
     public static Vec2 zero() {
         return null;
     }
+
     public static Vec2 of(double x, double y) {
         return null;
     }
