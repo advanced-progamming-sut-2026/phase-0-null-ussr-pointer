e4dccd447913ed07ea3db831fede7b3667346273
rezahazrati <rhazrati44@gmail.com>
2026-07-12 07:45:33 +0330

    minor fix

--- src/com/ussr/pvz/view/mainmenu/ProfileMenu.java
+++ src/com/ussr/pvz/view/mainmenu/ProfileMenu.java
@@ -1,12 +1,23 @@
 package com.ussr.pvz.view.mainmenu;
 
+import com.ussr.pvz.controller.GlobalController;
+import com.ussr.pvz.controller.maincontroller.ProfileController;
 import com.ussr.pvz.view.AppMenu;
 
 import java.util.Scanner;
 
 public class ProfileMenu implements AppMenu {
+    private final GlobalController globalController = new GlobalController();
+    private final ProfileController profileController = new ProfileController();
+
     @Override
     public void run(Scanner scanner) {
-
+        String input = scanner.nextLine();
+        String output = globalController.handleCommand(input);
+        if (output.isEmpty()) {
+            System.out.println(profileController.handleCommand(input));
+        } else {
+            System.out.println(output);
+        }
     }
 }
